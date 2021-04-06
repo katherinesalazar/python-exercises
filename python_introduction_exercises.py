24463f7b32cd691e2cb8dba74a3fa234a07ac0c1
katherinesalazar <47463674+katherinesalazar@users.noreply.github.com>
2021-04-06 14:37:28 -0500

    added code

---
+++ python_introduction_exercises.py
@@ -0,0 +1,8 @@
+print ('Hello, World!')
+
+greeting = ('This is my Hello, World greeting variable')
+
+print(greeting)
+
+shortcut_1 = ('Cmd + Shift + P will bring up the shortcuts in Jupyter')
+print (shortcut1)added source code added
