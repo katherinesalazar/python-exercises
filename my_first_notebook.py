3eeb54c1c3df7d0fb090bd751552377d9e471f8b
katherinesalazar <47463674+katherinesalazar@users.noreply.github.com>
2021-04-09 14:04:26 -0500

    added source code

---
+++ my_first_notebook.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#  Test out the jupyter notebook environment: execute at least 3 Python cells, 
+# and make some notes that describe 3 shortcuts for jupyter notebooks.
+
+
+# In[9]:
+
+
+shortcut1 = ('Cmd + Shift + P will bring up the shortcuts in Jupyter')
+print(shortcut1)
+
+
+# In[10]:
+
+
+shortcut2 = ('Shift + Enter will run the current cell in Jupyter')
+print(shortcut2)
+
+
+# In[8]:
+
+
+shortcut3 = ('Esc + s will save a notebook in Jupyter')
+print(shortcut3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
